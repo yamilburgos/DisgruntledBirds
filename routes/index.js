e5805cae788caab14288d7a2f88a42d50d1991d4
Student <student@GAs-MacBook-Air-2.local>
2017-04-23 13:06:00 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+var fs = require("fs");
+var angryObject = JSON.parse(fs.readFileSync("angry_birds.json"));
+var url = require('url');
+var _ = require("underscore");
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Angry Birds' });
+});
+
+router.get('/angry-birds', function(req, res, next) {
+	var queryObject = url.parse(req.url, true).query;
+	var passedBirds = _.where(angryObject, queryObject);
+
+	console.log("This is ", queryObject);
+	console.log("Testing: ", passedBirds);
+
+ 	res.render('allBirds', { 
+  		title: 'All Angry Birds',
+ 		array: passedBirds
+ 	});
+});
+
+router.get('/angry-birds/:id', function(req, res, next) {
+	var id = req.params.id;
+ 	res.render('thisBird', { entry: angryObject[id] });
+});
+
+module.exports = router;
